7f168c8e8bce89d6efda332d70dfff109362bb92
John <John@10.126.13.117>
2018-05-29 18:18:24 -0500

    inicio

---
+++ src/main/java/co/com/poli/AppRegistroMaterias/model/Materia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.poli.AppRegistroMaterias.model;
+
+/**
+ *
+ * @author John
+ */
+public class Materia {
+    private String idMateria;
+    private String nombre;
+    private double numCreditos;
+    private double cupoMin;
+    private double cupoMax;
+    
+}
